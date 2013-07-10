a8d58131346b57efe3cc68480b6adddf64c74290
Ubuntu <ubuntu@ip-172-31-28-181.us-west-2.compute.internal>
2013-07-10 02:45:48 +0000

    revised grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,8 +66,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <file_url>', 'URL of file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson
+
+    if(program.file)
+     	checkJson = checkHtmlFile(program.file, program.checks);
+    else if(program.url) {
+	rest.get(program.url).on(complete, function(result){
+	   checkJson = checkHtmlFile(result, program.checks)
+	});
+    	
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
